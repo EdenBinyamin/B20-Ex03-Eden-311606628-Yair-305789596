ef16db547f439abaf6f9f9162e0858f3f3b38ec8
Eden Binyamin <edenbi@mta.ac.il>
2020-05-30 15:27:00 +0300

    vczxv

---
+++ B20 Ex03 Eden 311606628 Yair 305789596/Wheel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B20_Ex03_Eden_311606628_Yair_305789596
+{
+    class Wheel
+    {
+    }
+}
